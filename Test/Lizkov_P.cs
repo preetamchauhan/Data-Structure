e9c7b1736d15a2508f2e8156055dbd49d1f7d143
preetamchauhan <preetam.chauhan@gmail.com>
2022-03-12 12:30:22 +0530

    Add project files.

---
+++ Test/Lizkov_P.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Lizkov_P
+    {
+    }
+
+    public interface ICredit
+    {
+        void Credit(int amount);
+    }
+    public interface IBanking
+    {
+        void Debit(int amount);
+    }
+
+    public class Transaction
+    {
+        internal List<IBanking> account = new List<IBanking>();
+        public Transaction()
+        {
+            this.account.Add(new SavingAccount(100));
+            this.account.Add(new PremiumAccount(200));
+        }
+        
+        public void DebitAnnualFee(List<IBanking> account)
+        {
+            account.ForEach(acc =>
+            {
+                    acc.Debit(5);
+            });
+        }      
+    }
+
+    public class SavingAccount : IBanking, ICredit
+    {
+        int Balance { get; set; }
+        public SavingAccount(int balance)
+        {
+            Balance = balance;
+        }
+        public void Credit(int amount)
+        {
+            Balance = Balance + amount;
+        }
+
+        public void Debit(int amount)
+        {
+            Balance = Balance - amount;
+        }
+    }
+
+    public class LongRunningAccount : ICredit
+    {
+        int Balance { get; set; }
+        public void Credit(int amount)
+        {
+            Balance = Balance + amount;
+        }
+    }
+    public class PremiumAccount : IBanking, ICredit
+    {
+        int Balance { get; set; }
+        int CreditPoints;
+        public PremiumAccount(int balance)
+        {
+            Balance = balance;
+        }
+        public void Credit(int amount)
+        {
+            Balance = Balance + amount;
+            AddCredits();
+        }
+
+        public void Debit(int amount)
+        {
+            Balance = Balance - amount;
+        }
+
+        public void AddCredits()
+        {
+            CreditPoints++;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "preetam";
+            HashSet<char> s = new HashSet<char>();
+            bool isFound = true;
+            for(int i=0; i< name.Length; i++)
+            {
+                for(int j=i+1; j< name.Length; j++)
+                {
+                    if(name[i] == name[j])
+                    {
+                        
+                    }
+                    
+                }
+            }
+        }
+    }
+}
